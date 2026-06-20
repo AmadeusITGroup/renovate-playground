eee46949f3bca0da0ab0b4d1acddaa6233929de3
MSI\mvgad <allabouttech0803@gmail.com>
2026-06-20 22:17:08 +0530

    feat: update TypeScript configuration and add pnpm workspace file
    - Changed module resolution from "node" to "bundler" in tsconfig.base.json
    - Updated TypeScript target from "es2017" to "es2022"
    - Modified lib array in tsconfig.base.json to include "es2022"
    - Reformatted paths and exclude arrays for better readability in tsconfig.base.json
    - Added a new pnpm-workspace.yaml file to manage workspace dependencies

--- jest.preset.js
+++ jest.preset.js
@@ -4,16 +4,10 @@ module.exports = {
   ...nxPreset,
   testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
   transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest',
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
   },
   resolver: '@nx/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageReporters: ['html'],
   maxWorkers: 1,
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-    },
-  },
 };
